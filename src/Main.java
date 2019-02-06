959298d8a4df9916c2d541bbe17690d3c160f035
johannathall <johannathall96@gmail.com>
2019-02-06 14:13:49 +0100

    Testar

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Testing2");
-        System.out.println("Testing again");  // test for a new branch
+        System.out.println("Testing again");  
     }
 }
